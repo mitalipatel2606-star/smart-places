8834fa965cb167abfac8fc56f10d3ad733ad3d7e
Mitali Patel <mitalipatel2606@gmail.com>
2025-12-17 21:40:40 +0530

    Fix Leaflet pins + optional improvements

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,17 @@ import {
   useMap,
 } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+// Fix for default marker icon not showing in Vite
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: new URL("leaflet/dist/images/marker-icon-2x.png", import.meta.url).href,
+  iconUrl: new URL("leaflet/dist/images/marker-icon.png", import.meta.url).href,
+  shadowUrl: new URL("leaflet/dist/images/marker-shadow.png", import.meta.url).href,
+});
+
 import "./style.css";
 
 /* ---------- Constants ---------- */
